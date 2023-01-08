fd7ed12e9c220a781c9eaaf36e5e56d4e4dcc38b
Dimi Kot <dimi.kot.code@gmail.com>
2023-01-07 22:21:11 -0800

    Implement weight-heuristic-based micro-shards rebalance

--- src/__tests__/rebalance.test.ts
+++ src/__tests__/rebalance.test.ts
@@ -1,94 +1,215 @@
+import flatten from "lodash/flatten";
+import sum from "lodash/sum";
+import sumBy from "lodash/sumBy";
+import { sprintf } from "sprintf-js";
 import rebalance from "../rebalance";
 
-test("rebalance", async () => {
-  runSnapshot([]);
-  runSnapshot([["m1", [1]]]);
-  runSnapshot([
-    ["m1", [1]],
-    ["m2", []],
-  ]);
-  runSnapshot([["m1", [1, 2]]]);
-  runSnapshot([
-    ["m1", [1, 2]],
-    ["m2", []],
-  ]);
-  runSnapshot([
-    ["m1", [1, 2]],
-    ["m2", [3]],
-  ]);
-  runSnapshot([
-    ["m1", [1]],
-    ["m2", [2, 3]],
-  ]);
-  runSnapshot([
-    ["m1", [1, 2, 3, 4]],
-    ["m2", []],
-  ]);
-  runSnapshot([
-    ["m1", [1, 2, 3, 4]],
-    ["m2", []],
-    ["m3", []],
-  ]);
-  runSnapshot([
-    ["m1", [1, 2, 3]],
-    ["m2", []],
-    ["m3", []],
-    ["m4", []],
-  ]);
-  runSnapshot([
-    ["m1", [1, 2, 3]],
-    ["m2", []],
-    ["m3", []],
-    ["m4", []],
-    ["m5", []],
-  ]);
-  runSnapshot(
-    [
-      ["m1", [1, 2]],
-      ["m2", [3, 4]],
-      ["m3", [5, 6]],
+test("real-life", async () => {
+  runSnapshot("real-life", {
+    m1: [],
+    m2: [
+      3102510, 2310389, 3431636, 2812913, 2809915, 3373267, 3533675, 2904572,
+      3028968, 3763126, 3330395, 5513279, 4499457, 2620674, 4204222, 3068099,
+      4044060, 2590400, 3398307, 3549054, 2705941, 2446029, 2543947, 3043896,
+      3185788, 3687653, 3011137, 3123301, 2968407, 2878138, 4008534, 2606604,
+      3583219, 3103622, 3296268, 2898755, 3429745, 2704922, 3047716, 4353234,
+      2977721, 2952326, 3165026, 5621594, 2866982, 2963043, 3029183, 3165786,
     ],
-    ["m3"]
-  );
-  runSnapshot(
-    [
-      ["m1", [1, 2]],
-      ["m2", [3, 4]],
-      ["m3", [5, 6]],
+    m3: [],
+    m4: [
+      2941652, 2259896, 2897068, 2441056, 2173301, 2557906, 3189854, 3262413,
+      3596507, 2850071, 2742650, 3007119, 3146139, 2732076, 2782602, 2843946,
+      2971959, 2650268, 4630436, 2509163, 2823596, 2496160, 4094308, 3715544,
+      3780254, 2473756, 3902806, 3177622, 2752508, 2610157, 2460287, 3588799,
+      3315124, 3386548, 2841916, 3389682, 2882752, 2721151, 2898093, 2891522,
+      3063611, 2924353, 3016623, 2736916, 2567722, 2250799, 3290465, 2302616,
+      3266051, 2737137, 3368923, 2739823, 2457847, 2588240, 3388789, 3253895,
+      2748270, 2490298, 3042848, 2685534, 2657002,
     ],
-    ["m2", "m3"]
+    m5: [
+      2402111, 3635241, 3494375, 4003589, 2644282, 3792954, 3031608, 2736750,
+      11682545, 2537989, 2789315, 3576189, 2573423, 2999839, 3195822, 2850765,
+      3003699, 2800095, 3373183, 3192320, 2466714, 3070357, 2320580, 2835098,
+      3759990, 2856268, 2578693, 2924938, 4521645, 2940125, 3526950, 2532835,
+      2867802, 2786013, 3572643, 3286283, 4558305, 6562024, 3357344, 2560850,
+      3622079, 2700293, 2638270, 2358971, 2690906, 2737271, 2679169, 2896335,
+      5173294, 3241496, 2641623, 2708660, 3026319, 3018266, 2776485, 3897435,
+      3933529, 2839020, 3071322, 4652406, 3633723, 3720616, 3242648, 2349381,
+      2801277, 3116048, 3872269, 3145946, 2958505, 3479378, 4414286, 3757053,
+      2871581, 2612754, 2440424, 2705234, 3717549, 4245181, 3699579, 2762198,
+      2877770, 2926338, 3206583, 3513134, 2683638, 2673500, 2367037, 2968401,
+      3413508, 2307521, 3420950, 2500139, 2308110, 2532689, 2875158, 3639822,
+      2573277, 2886183, 3512852, 3652854, 3666576, 2831328,
+    ],
+    m6: [
+      3210829, 2599238, 3261651, 3013344, 2994142, 2817397, 2914849, 3466772,
+      2798274, 3160658, 4189071, 60846732, 2774519, 2921195, 3273649, 2762413,
+      3398789, 5174865, 3458114, 3212180, 3243874, 2420268, 2914613, 3460018,
+      3238051, 3099167, 3156420, 3208024, 3609000, 3743240, 3784360, 2959994,
+      2997000, 3412687, 2478898, 3143575, 3009686, 3180545, 3334735, 3322849,
+      3031021, 2760785, 3364934, 2902034, 2752392, 3147604, 2464625, 2488516,
+      2844696, 3120980, 2783978, 3106528, 2295123, 3116163, 2671385, 2900650,
+      2700870, 2613138, 2724027, 3489817, 3009028, 3005714, 2825044, 2761433,
+      2746928, 4153615, 3035247, 3166575, 3125205, 3486267, 3436957, 2730814,
+      3037462, 3305552, 2706198, 2957812, 3715174, 3008117, 3177874, 2779452,
+      3149109, 2842873, 3746175, 3403372, 2711371, 3976340, 3468210, 3180945,
+    ],
+  });
+});
+
+test("synthetic", async () => {
+  runSnapshot("empty", {});
+
+  runSnapshot("no-op 1", {
+    m1: [1],
+  });
+
+  runSnapshot("no-op 2", {
+    m1: [1],
+    m2: [],
+  });
+
+  runSnapshot("no-op 2", {
+    m1: [],
+    m2: [1],
+  });
+
+  runSnapshot("move smallest out", {
+    m1: [1, 2],
+    m2: [],
+  });
+
+  runSnapshot("move largest out", {
+    m1: [1, 2, 3, 6],
+    m2: [],
+  });
+
+  runSnapshot("move many small out", {
+    m1: [1, 2, 3, 7],
+    m2: [],
+  });
+
+  runSnapshot("huge shards", {
+    m1: [10000, 100, 1, 2, 3, 4, 5],
+    m2: [],
+    m3: [],
+    m4: [],
+    m5: [],
+  });
+
+  runSnapshot("zero-weight shards only", {
+    m1: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    m2: [],
+    m3: [],
+    m4: [],
+  });
+
+  runSnapshot("zero-weight shards and non-empty", {
+    m1: [1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0],
+    m2: [],
+    m3: [],
+    m4: [],
+  });
+
+  runSnapshot(
+    "decommission of one",
+    {
+      m1: [1, 2],
+      m2: [3, 4],
+      m3: [5, 6],
+    },
+    ["m2"]
   );
+
+  runSnapshot(
+    "decommission of two",
+    {
+      m1: [1, 2],
+      m2: [3, 4],
+      m3: [5, 6],
+    },
+    ["m1", "m2"]
+  );
+
   await expect(
     (async () =>
       runSnapshot(
-        [
-          ["m1", [1, 2]],
-          ["m2", [3, 4]],
-          ["m3", [5, 6]],
-        ],
+        "bad decommission of all",
+        {
+          m1: [1, 2],
+          m2: [3, 4],
+          m3: [5, 6],
+        },
         ["m1", "m2", "m3"]
       ))()
-  ).rejects.toThrow(Error);
+  ).rejects.toThrow(/all islands/);
+
+  runSnapshot("two zero weight and two equal weight", {
+    m1: [0, 134, 134, 0],
+    m2: [],
+    m3: [],
+    m4: [],
+  });
 });
 
+let counter = 0;
+
 function runSnapshot(
-  input: Array<[string, number[]]>,
-  decommissionDBs: string[] = []
+  name: string,
+  islandsFrom: Record<string, number[]>,
+  decommissionNames: string[] = []
 ): void {
-  const res = rebalance(new Map(input), decommissionDBs);
-  const inputStr = input.map(([db, shards]) => `  ${db}: [${shards}]\n`);
-  const outputStr = [...res.shardsByDBs.entries()].map(
-    ([db, shards]) => `  ${db}: [${shards}]\n`
+  const islandNames = Object.keys(islandsFrom);
+
+  const islands = new Map(
+    Object.values(islandsFrom).map((weights, islandNo) => [
+      islandNo,
+      weights.map((weight) => ({ weight })),
+    ])
   );
-  const movesStr = res.moves.map(
-    ({ from, to, shard }) => `  ${shard}: ${from}->${to}\n`
+  const moves = rebalance(
+    islands,
+    decommissionNames.map((name) => islandNames.indexOf(name))
   );
+  const islandsTo = Object.fromEntries(
+    [...islands.entries()].map(
+      ([islandNo, shards]) =>
+        [islandNames[islandNo], shards.map(({ weight }) => weight)] as const
+    )
+  );
+
+  const desiredWeight =
+    islandNames.length > 0
+      ? sum(flatten(Object.values(islandsFrom))) / islandNames.length
+      : 0;
   expect(
     "\n" +
-      inputStr.join("") +
+      `  desiredWeight=${desiredWeight}\n` +
       "===\n" +
-      movesStr.join("") +
+      inspectIslands(islandsFrom) +
+      "==>\n" +
+      inspectIslands(islandsTo) +
       "===\n" +
-      outputStr.join("")
-  ).toMatchSnapshot();
+      `  ${sumBy(moves, ({ shards }) => shards.length)} move(s):\n` +
+      moves
+        .map(
+          ({ from, to, shards }) =>
+            `  - ${islandNames[from]}->${islandNames[to]}: [` +
+            shards.map(({ weight }) => weight) +
+            "]\n"
+        )
+        .join("")
+  ).toMatchSnapshot(`${sprintf("%02d", counter++)}: ${name}`);
+}
+
+function inspectIslands(islands: Record<string, number[]>): string {
+  const totalWeight = sum(flatten(Object.values(islands))) || 1;
+  return Object.entries(islands)
+    .map(
+      ([islandName, weights]) =>
+        `  ${islandName}: sum=${sprintf("%-10d", sum(weights))} ` +
+        `(${sprintf("%.2f", (sum(weights) / totalWeight) * 100)}%) ` +
+        `[${weights}]\n`
+    )
+    .join("");
 }
--- src/rebalance.ts
+++ src/rebalance.ts
@@ -1,73 +1,362 @@
+import assert from "assert";
+import compact from "lodash/compact";
 import difference from "lodash/difference";
-import range from "lodash/range";
+import flatten from "lodash/flatten";
+import groupBy from "lodash/groupBy";
+import minBy from "lodash/minBy";
+import sortBy from "lodash/sortBy";
 import sumBy from "lodash/sumBy";
+import without from "lodash/without";
 
-export interface Move<TDB, TShard> {
-  from: TDB;
-  to: TDB;
-  shard: TShard;
+const SHARD_WEIGHT_MOVE_FROM_OVERLOADED_TO_OVERLOADED_FACTOR = 0.75;
+const DEFAULT_FRACTION_OF_MEDIAN_TO_CONSIDER_EMPTY = 0.1;
+
+interface Shard {
+  weight: number;
+}
+
+interface Island {
+  no: number;
+  sumWeight: number;
+  shards: Set<Shard>;
 }
 
 /**
- * Returns shards rebalance plan for the client to execute to distribute shards
- * evenly across databases. If a DB is passed in decommissionDBs, then all the
- * shards from it will be migrated out.
+ * Accepts a list of islands (an island is a collection of shards with different
+ * weights). Modifies it to make the shards being distributed more fairly.
+ * Returns the list of shards moves.
+ *
+ * ChatGPT mentions several related academical problems:
+ * - "linear partitioning"
+ * - "balanced partition"
+ * - "partition problem"
+ *
+ * Unfortunately, none of the above approaches works out of the box due to
+ * corner cases and heuristics we have.
+ *
+ * The current algorithm implemented is not perfect intentionally: it doesn't
+ * try to achieve a fully-fair distribution, because it would produce way too
+ * many moves otherwise (shards moves are expensive). Instead, it makes
+ * trade-offs:
+ * 1. Tries to unload the largest "overloaded" shards to the smallest island if
+ *    such island wouldn't appear overloaded after that.
+ * 2. If there is no such island in (1) (i.e. no matter where we move it, the
+ *    destination will be overloaded), it may still move the shard to the
+ *    smallest island, BUT only it the final benefit of the move would be more
+ *    than SHARD_WEIGHT_MOVE_FROM_OVERLOADED_TO_OVERLOADED_FACTOR fraction of
+ *    the shard's weight. (We don't want to pay the price for a move which won't
+ *    move the needle much.)
+ * 3. Compensates large shards relocations with the corresponding number of
+ *    small shard relocations, so largest shards and smallest shards are
+ *    redistributed more or less in sync with each other, and there will be e.g.
+ *    no island with just 2 biggest shards, whilst other islands have tens of
+ *    them.
+ * 4. Also, a special treatment is applied to "empty" shards. They are treated
+ *    as "filled in the future". The shard is considered "empty" if its weight
+ *    is less than fractionOfMedianToConsiderEmpty of a median shard's weight.
+ *    Such shards are distributed uniformly among the islands, not looking at
+ *    their weights; this prevents the situation when most of the "empty" shards
+ *    appear on the same island in the end.
+ *
+ * All those trade-offs produce a slightly imbalanced result. In real life, it
+ * doesn't matter much though, because shards sizes are more or less equal.
  */
-export default function rebalance<TDB, TShard>(
-  shardsByDBs: ReadonlyMap<TDB, TShard[]>,
-  decommissionDBs: TDB[] = []
-): {
-  shardsByDBs: ReadonlyMap<TDB, TShard[]>;
-  moves: Array<Move<TDB, TShard>>;
-} {
-  const numShards = sumBy([...shardsByDBs.values()], (shards) => shards.length);
-  const map = [...shardsByDBs.entries()].map(([db, shards]) => ({
-    db,
-    needShards: 0,
-    shards: [...shards],
-  }));
+export default function rebalance<TShard extends Shard>(
+  islands: Map<number, readonly TShard[]>,
+  decommissionIslandNos: number[] = [],
+  fractionOfMedianToConsiderEmpty = DEFAULT_FRACTION_OF_MEDIAN_TO_CONSIDER_EMPTY
+): Array<{ from: number; to: number; shards: TShard[] }> {
+  const islandsFrom = new Map<number, Island>(
+    [...islands.entries()].map(([no, shards]) => [
+      no,
+      {
+        no,
+        sumWeight: sumBy(shards, ({ weight }) => weight),
+        shards: new Set(shards),
+      },
+    ])
+  );
 
-  const finalDBs = difference([...shardsByDBs.keys()], decommissionDBs);
-  if (shardsByDBs.size > 0 && finalDBs.length === 0) {
-    throw Error(
-      "You can't decommission all of the DBs, at least one should remain"
+  const islandsTo = new Map(
+    [...islandsFrom.entries()].map(([no, island]) => [
+      no,
+      { ...island, shards: new Set([...island.shards]) },
+    ])
+  );
+  decommissionIslands(islandsTo, decommissionIslandNos);
+  const emptyShards = extractEmptyShards(
+    islandsTo,
+    fractionOfMedianToConsiderEmpty
+  );
+  balanceIslands(islandsTo);
+  injectShardsUniformly(islandsTo, emptyShards);
+
+  const moves = buildMoves(islandsFrom, islandsTo);
+
+  const shardsTo = new Map(
+    [...islandsTo.values()].map(({ no, shards }) => [
+      no,
+      [...shards] as TShard[],
+    ])
+  );
+
+  const shardsFromOrder = new Map(
+    flatten([...islandsFrom.values()].map(({ shards }) => [...shards])).map(
+      (shard, i) => [shard, i]
+    )
+  );
+
+  islands.clear();
+  for (const islandNo of islandsFrom.keys()) {
+    islands.set(
+      islandNo,
+      sortBy(
+        shardsTo.get(islandNo) ?? [],
+        (shard) => shardsFromOrder.get(shard) ?? Infinity
+      )
     );
   }
 
-  const chunks = evenChunks(range(0, numShards), finalDBs.length);
-  for (const entry of map) {
-    entry.needShards = decommissionDBs.includes(entry.db)
-      ? 0
-      : chunks.shift()!.length;
+  return Object.values(groupBy(moves, ({ from, to }) => `${from}:${to}`)).map(
+    (moves) => ({
+      from: moves[0].from,
+      to: moves[0].to,
+      shards: moves.map(({ shard }) => shard as TShard),
+    })
+  );
+}
+
+/**
+ * Performs islands re-balancing in-place.
+ */
+function balanceIslands(islands: Map<number, Island>): void {
+  if (islands.size === 0) {
+    return;
   }
 
-  const moves: Array<Move<TDB, TShard>> = [];
-  for (const from of map) {
-    const shardsToMove = from.shards.splice(from.needShards);
-    for (const shard of shardsToMove) {
-      for (const to of map) {
-        if (to.shards.length < to.needShards) {
-          moves.push({ from: from.db, to: to.db, shard });
-          to.shards.push(shard);
-          break;
+  const shardsSortedDesc = sortBy(
+    flatten(
+      [...islands.values()].map((island) =>
+        [...island.shards].map((shard) => [shard, island] as const)
+      )
+    ),
+    ([shard]) => -1 * shard.weight
+  );
+
+  const desiredIslandWeight =
+    sumBy(shardsSortedDesc, ([{ weight }]) => weight) / islands.size;
+
+  let bigShardMovedWeight = 0;
+
+  while (shardsSortedDesc.length > 0) {
+    // First, try to evacuate ONE biggest shard of an OVERLOADED island to the
+    // smallest island ONLY if it wouldn't overload it. Do it until we actually
+    // succeed moving ONE shard (and skip the shards we couldn't find the new
+    // "better" island for).
+    while (shardsSortedDesc.length > 0) {
+      const [shard, islandFrom] = shardsSortedDesc.shift()!; // biggest shard
+      if (islandFrom.sumWeight > desiredIslandWeight) {
+        // The island of this shard is overloaded, so try to move the shard off
+        // to OTHER SMALLEST island.
+        const islandTo = minBy(
+          without([...islands.values()], islandFrom),
+          ({ sumWeight }) => sumWeight
+        );
+        if (islandTo) {
+          const islandToNewWeight = islandTo.sumWeight + shard.weight;
+          if (
+            islandToNewWeight <= desiredIslandWeight ||
+            islandFrom.sumWeight - islandToNewWeight >
+              shard.weight *
+                SHARD_WEIGHT_MOVE_FROM_OVERLOADED_TO_OVERLOADED_FACTOR
+          ) {
+            // The new island would NOT be overloaded after the move, so it's
+            // better than the currently overloaded island.
+            moveShard({ shard, islandFrom, islandTo });
+            bigShardMovedWeight += shard.weight;
+            break;
+          }
         }
       }
     }
-  }
 
-  return {
-    shardsByDBs: new Map(map.map(({ db, shards }) => [db, shards])),
-    moves,
-  };
+    // Then, to compensate this biggest shard relocation, redistribute N
+    // SMALLEST shards of OVERLOADED islands to the smallest islands up until we
+    // accumulate the total weight of the biggest shard we have just
+    // redistributed.
+    while (shardsSortedDesc.length > 0) {
+      const [shard, islandFrom] = shardsSortedDesc[shardsSortedDesc.length - 1]; // smallest shard
+      if (islandFrom.sumWeight > desiredIslandWeight) {
+        if (bigShardMovedWeight >= shard.weight) {
+          const islandTo = minBy(
+            without([...islands.values()], islandFrom),
+            ({ sumWeight }) => sumWeight
+          );
+          if (
+            islandTo &&
+            islandTo.sumWeight < islandFrom.sumWeight &&
+            score(islands, { shard, islandFrom, islandTo }) > score(islands)
+          ) {
+            shardsSortedDesc.pop();
+            moveShard({ shard, islandFrom, islandTo });
+            bigShardMovedWeight -= shard.weight;
+            continue;
+          }
+        }
+      }
+
+      break;
+    }
+  }
 }
 
-function evenChunks<T>(input: T[], n: number): T[][] {
-  const ret = Array(n);
-  for (let i = 0, j = 0; i < ret.length; i++) {
-    const sliceEnd = Math.round((i + 1) * (input.length / n));
-    ret[i] = input.slice(j, sliceEnd);
-    j = sliceEnd;
+/**
+ * Removes islands which need to be decommissioned from the list of islands and
+ * redistributes their shards uniformly (without looking at their weights).
+ */
+function decommissionIslands(
+  islands: Map<number, Island>,
+  islandNos: number[]
+): void {
+  if (islands.size === 0) {
+    return;
   }
 
-  return ret;
+  const shards: Shard[] = [];
+  for (const [no, island] of islands) {
+    if (islandNos.includes(no)) {
+      shards.push(...island.shards);
+      islands.delete(no);
+    }
+  }
+
+  if (islands.size === 0) {
+    throw Error(
+      "Can't decommission all islands: we need at least one remaining to put all the shards on"
+    );
+  }
+
+  injectShardsUniformly(islands, shards);
+}
+
+/**
+ * Removes shards smaller than a threshold from the islands and returns the
+ * removed shards.
+ */
+function extractEmptyShards(
+  islands: Map<number, Island>,
+  fractionOfMedianToConsiderEmpty: number
+): Shard[] {
+  const shardsSortedAsc = sortBy(
+    flatten([...islands.values()].map((island) => [...island.shards])),
+    ({ weight }) => weight
+  );
+  if (shardsSortedAsc.length === 0) {
+    return [];
+  }
+
+  const weightMedian =
+    shardsSortedAsc[Math.trunc(shardsSortedAsc.length / 2)].weight;
+  const weightThreshold = weightMedian * fractionOfMedianToConsiderEmpty;
+
+  const emptyShards = [];
+  for (const island of islands.values()) {
+    const empty = [...island.shards].filter(
+      ({ weight }) => weight <= weightThreshold
+    );
+    island.shards = new Set(difference([...island.shards], empty));
+    emptyShards.push(...empty);
+  }
+
+  return emptyShards;
+}
+
+/**
+ * Injects shards to the list of islands uniformly, not considering their
+ * weights.
+ */
+function injectShardsUniformly(
+  islands: Map<number, Island>,
+  shards: Shard[]
+): void {
+  const islandList = [...islands.values()];
+  for (const shard of shards) {
+    islandList[0].shards.add(shard);
+    islandList.push(islandList.shift()!);
+  }
+}
+
+/**
+ * Builds the list of moves which would turn one list of islands into another
+ * list of islands.
+ */
+function buildMoves(
+  from: Map<number, Island>,
+  to: Map<number, Island>
+): Array<{ from: number; to: number; shard: Shard }> {
+  const shardsToIsland = new Map(
+    flatten(
+      [...to.values()].map((island) =>
+        [...island.shards].map((shard) => [shard, island] as const)
+      )
+    )
+  );
+  return flatten(
+    [...from.values()].map((from) =>
+      compact(
+        [...from.shards].map((shard) => {
+          const to = shardsToIsland.get(shard)!;
+          return from.no === to.no
+            ? null
+            : {
+                from: from.no,
+                to: shardsToIsland.get(shard)!.no,
+                shard,
+              };
+        })
+      )
+    )
+  );
+}
+
+/**
+ * Score of the cluster is the minimal sum weight among its islands. The bigger
+ * the score, the more fair is the distribution.
+ */
+function score(
+  islands: Map<number, Island>,
+  afterMove?: {
+    shard: Shard;
+    islandFrom: Island;
+    islandTo: Island;
+  }
+): number {
+  return Math.min(
+    ...[...islands.values()].map(
+      (island) =>
+        island.sumWeight -
+        (afterMove?.islandFrom === island ? afterMove.shard.weight : 0) +
+        (afterMove?.islandTo === island ? afterMove.shard.weight : 0)
+    )
+  );
+}
+
+/**
+ * Moves a shard from one island to another updating the island's sum weight.
+ */
+function moveShard({
+  shard,
+  islandFrom,
+  islandTo,
+}: {
+  shard: Shard;
+  islandFrom: Island;
+  islandTo: Island;
+}): void {
+  assert(islandFrom.shards.has(shard));
+  assert(!islandTo.shards.has(shard));
+  islandFrom.shards.delete(shard);
+  islandFrom.sumWeight -= shard.weight;
+  islandTo.shards.add(shard);
+  islandTo.sumWeight += shard.weight;
 }
