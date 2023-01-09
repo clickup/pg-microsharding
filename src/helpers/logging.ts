4a7ac5710cce7d202da08f9d89a7561e453dd9d3
Dimi Kot <dimi.kot.code@gmail.com>
2023-01-09 00:45:34 -0800

    Run shards rebalancing tool in parallel tmux panes

--- src/helpers/logging.ts
+++ src/helpers/logging.ts
@@ -3,7 +3,11 @@ import logUpdate from "log-update";
 export const progress = logUpdate.create(process.stdout, { showCursor: true });
 
 export function log(msg: string): void {
+  const date = new Date()
+    .toString()
+    .replace(/ GMT.*/, "")
+    .replace(/ \(.*/, "");
   progress.done();
-  progress(msg);
+  progress(msg.replace(/^/gm, `[${date}] `));
   progress.done();
 }
